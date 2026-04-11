6d692aa6d258c2bce723f364e9db534aced6307a
Sawyer <sawyer.kocourek@gmail.com>
2026-04-11 00:48:12 -0500

    Picks random passenger

--- Scenes/Objectives/Spawner.cs
+++ Scenes/Objectives/Spawner.cs
@@ -4,10 +4,16 @@ using System.Collections.Generic;
 
 public partial class Spawner : Node3D
 {
+	public enum PassengerTypes
+	{
+		person,
+		Bloblin
+	}
 	// Called when the node enters the scene tree for the first time.
 	PackedScene Person = GD.Load<PackedScene>("res://Scenes/Objectives/StrandedPerson.tscn");
 	public Texture2D[] Icons = {
-		GD.Load<Texture2D>("res://Assets/Textures/Passenger.png")
+		GD.Load<Texture2D>("res://Assets/Textures/Passenger.png"),
+		GD.Load<Texture2D>("res://Assets/Textures/Bloblin.png"),
 	};
 	public Passenger.Destinations randomDestination()
 	{
@@ -40,10 +46,21 @@ public partial class Spawner : Node3D
 		{
 			return;
 		}
+		PassengerTypes type = (PassengerTypes)(GD.Randi() % Enum.GetNames(typeof(PassengerTypes)).Length);
 		Passenger newPassenger = Person.Instantiate<Passenger>();
 		Passenger.Destinations destinations = randomDestination();
 		AddChild(newPassenger);
-		newPassenger.instantiatePassenger(randomIcon(), destinations,randomMessage(destinations));
+		newPassenger.instantiatePassenger(Icons[(int)type], destinations,randomMessage(destinations));
+		switch (type){
+			case PassengerTypes.person:
+				newPassenger.GetNode<Node3D>("Person").GetNode<Node3D>("PersonToSave").Visible = true;
+				newPassenger.GetNode<Node3D>("Person").GetNode<Node3D>("SpaceBloblin").Visible = false;
+				break;
+			case PassengerTypes.Bloblin:
+				newPassenger.GetNode<Node3D>("Person").GetNode<Node3D>("PersonToSave").Visible = false;
+				newPassenger.GetNode<Node3D>("Person").GetNode<Node3D>("SpaceBloblin").Visible = true;
+				break;
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
