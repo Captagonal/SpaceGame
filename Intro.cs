360f8cf2010da7b2b8c770ddae2606837a5d0db7
Sawyer <sawyer.kocourek@gmail.com>
2026-04-12 19:19:09 -0500

    IntroVideo!!

--- Intro.cs
+++ Intro.cs
@@ -3,9 +3,11 @@ using System;
 
 public partial class Intro : Control
 {
+	VideoStreamPlayer videoPlayer2;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		videoPlayer2 = GetNode<VideoStreamPlayer>("2");
 		var config = new ConfigFile();
 
 		// Load data from a file.
@@ -20,16 +22,86 @@ public partial class Intro : Control
 		var audioBus = AudioServer.GetBusIndex("Master");
 
 		AudioServer.SetBusVolumeDb(audioBus, Mathf.LinearToDb((float)config.GetValue("Audio", "Volume", 1f)));
+		transmissionLabel = GetNode<Label>("Transmission");
+		transmissionTimer = transmissionLabel.GetNode<Timer>("TransmissionTimer");
+		transmissionSound = transmissionLabel.GetNode<AudioStreamPlayer>("TransmissionSound");
+		transmissionTimer.Timeout += MakeMessage;
+		transmissionLabel.Text = "";
+		DisplayTransmission("Welcome to your new job");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	public void introFinished(){
+	public void part1Done()
+	{
+		videoPlayer2.Play();
+	}
+	public void introFinished()
+	{
+		GetTree().CreateTimer(5).Timeout += skipIntro;
+	}
+	public void skipIntro()
+	{
 		GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
 	}
-	public void skipIntro(){
-		GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
+
+	int messageIndex = 0;
+	const int charsOnScreen = 17;
+	char[] messageChars;
+	Label transmissionLabel;
+	Timer transmissionTimer;
+	AudioStreamPlayer transmissionSound;
+	public void DisplayTransmission(String message)
+	{
+		transmissionSound.Play();
+		// 
+		transmissionLabel.Show();
+
+		transmissionTimer.Start();
+		messageChars = message.ToCharArray();
+		messageIndex = 0;
+
+
+	}
+
+	public void MakeMessage()
+	{
+		// GD.Print("Transmission Timer Tick");
+		// GD.Print("Current Transmission Text: " + transmissionLabel.Text);
+		// GD.Print("Message Index: " + messageIndex);
+		if (messageIndex < messageChars.Length)
+		{
+			messageIndex++;
+			transmissionLabel.Text += messageChars[messageIndex - 1];
+			if (transmissionLabel.Text.Length > charsOnScreen)
+			{
+				transmissionLabel.Text = transmissionLabel.Text.Substring(transmissionLabel.Text.Length - charsOnScreen);
+			}
+		}
+		else
+		{
+			transmissionTimer.Stop();
+			// transmissionLabel.Hide();
+		}
+	}
+	int textIndex = 0;
+	public void nextText(){
+	switch(textIndex){
+		case 0:
+			DisplayTransmission("  With the advent of readily available space travel tons of new jobs opened up");
+			break;
+		case 1:
+			DisplayTransmission("  Some of them of them feel like small deals but really your saving lives");
+			break;
+		case 2:
+			DisplayTransmission("  Welcome to the Space Guard");
+			break;
+		default:
+			introFinished();
+			break;
+	}
+	textIndex++;
 	}
 }
